216c4e936bad5d23e0ebbba3dd2f3d9ddeb11a7a
Phisan Sookkhee <phisan.shukkhi@gmail.com>
2022-09-19 14:06:11 +0700

    setup web page

--- web/views.py
+++ web/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+
+def Home(request):
+    return HttpResponse("Hello, Home Page")
+
+
+def About(request):
+    return HttpResponse("Hello, About Page")
+
+
+def Contact(request):
+    return HttpResponse("Hello, Contact Page")
